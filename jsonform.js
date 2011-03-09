56ff225a5d033c95f261cff36d20c12ba01e032b
Christiano Milfont <cmilfont@gmail.com>
2011-03-09 16:20:37 -0300

    inicio do plugin

---
+++ jsonform.js
@@ -0,0 +1,19 @@
+/*!
+ * jsonform.js: for jQuery JsonForm Plugin 0.0.1
+ * Create standard JSON from Forms with jQuery.
+ * Requires jquery.js 
+ * https://github.com/milfont/jsonform
+ *
+ * Copyright Milfont Consulting.
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ */
+(function (jQuery) {
+
+  jQuery.fn.extend({
+    jsonform: function (selector, config) {
+      console.log("Funfou");
+    }
+  });
+  
+})(jQuery);
