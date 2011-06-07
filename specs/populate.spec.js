07cc29d853119e8e1c017748b1640422ff1dcb3d
Christiano Milfont <cmilfont@gmail.com>
2011-06-07 15:34:04 -0300

    passando o projeto para node atualizado

--- specs/populate.spec.js
+++ specs/populate.spec.js
@@ -46,29 +46,28 @@ var template =  "<form action='#' id='jsonform'> \
         <input type='submit' id='submit'/> \
       </p> \
     </form>";
+    
 jQuery(template).appendTo("body");
 
 var lancamento;
 
-describe('jsonform',function(){
-    
-  beforeEach(function () {
-    lancamento = {
-      empresa: {id: 2, name: "Teste"},
-      partidas: [
-        {conta: {codigo:"1.02.0002", nome: "Compras"}, natureza: "1"}, 
-        {conta: {codigo:"1.02.0001", nome: "Banco"}, natureza: "-1"}
-      ],
-      description: "Teste",
-      value: "125,67",
-      date: "12/03/1999"
-    };
-  });
-  
-    it('when exists nested objects', function () {
-      jQuery('#jsonform').jsonform(lancamento, function(json) {      
-        expect(jQuery("#partidas\\[0\\]\\.conta\\.codigo").val().toString()).toEqual("1.02.0002");
-      });
-    });
+describe('jsonform', function() {
 
+    beforeEach( function () {
+        lancamento = {
+            empresa: {id: 2, name: "Teste"},
+            partidas: [
+                {conta: {codigo:"1.02.0002", nome: "Compras"}, natureza: "1"},
+                {conta: {codigo:"1.02.0001", nome: "Banco"}, natureza: "-1"}
+            ],
+            description: "Teste",
+            value: "125,67",
+            date: "12/03/1999"
+        };
+    });
+    it('when exists nested objects', function () {
+        jQuery('#jsonform').jsonform(lancamento, function(json) {
+            expect(jQuery("#partidas\\[0\\]\\.conta\\.codigo").val().toString()).toEqual("1.02.0002");
+        });
+    });
 });
--- testrunner.js
+++
@@ -1,6 +0,0 @@
-var testrunner = require( "qunit" );
-testrunner.run({
-	deps: ".test/env.js",
-  code: "./lib/jsonform.js", 
-  tests:"./test/tests.js"
-});
