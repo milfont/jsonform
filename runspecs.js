384dca4ccad4dacf61cfbeba401c87b2ba2d2299
Christiano Milfont <cmilfont@gmail.com>
2011-06-08 11:11:12 -0300

    coloquei o template em um arquivo externo

--- runspecs.js
+++ runspecs.js
@@ -11,13 +11,51 @@ global.jQuery = require("jquery").create(window);
 //Test framework
 var jasmine=require('jasmine-node');
 
-for(var key in jasmine) {
-  global[key] = jasmine[key];
-}
-
 //What we're testing
 require(__dirname + "/lib/jsonform.js")
 
+var isVerbose = true;
+var showColors = true;
+
+// files 
+var fs = require('fs');
+var template_path = __dirname + '/specs/template.html';
+
+var template = fs.readFileSync(template_path).toString();
+
+global["template"] = template;
 jasmine.executeSpecsInFolder(__dirname + '/specs', function(runner, log){
+
+	/*
+	console.log("********");
+
+    var specs = runner.specs();
+    var specCount = 0;
+    for (var i = 0; i < specs.length; i++) {
+        console.log(specs[i].getFullName());
+    }
+
+    //console.log("Reporter: ", runner.suites_);
+    console.log("Results ***********");
+    var results = runner.results();
+    for (var i = 0; i < results.items_.length; i++) {
+    	var item = results.items_[i];
+        console.log(item.items_[0].description);
+    }
+    
+    
+    console.log("Suites ***********");
+    var suites = runner.suites();
+    for (var i = 0; i < suites.length; i++) {
+        var suite = suites[i];
+            var results = suite.results();
+            console.log("cade? ", results.items_);
+            for (var i = 0; i < results.items_.length; i++) {
+                var item = results.items_[i];
+                console.log(item.description);
+            }
+    }*/
+    
     process.exit(runner.results().failedCount?1:0);
-}, true, true);
+}, isVerbose, showColors);
+  
--- specs/populate.spec.js
+++ specs/populate.spec.js
@@ -1,57 +1,10 @@
-var template =  "<form action='#' id='jsonform'> \
-      <p>\
-        <label for='empresa.id'>Empresa:</label> \
-        <select id='empresa.id'> \
-          <option value='1'>Milfont Consulting</option> \
-          <option value='2'>TriadWorks</option> \
-          <option value='3'>Caelum</option> \
-        </select> \
-      </p> \
-      <p> \
-        <label for='date'>Data:</label>  <input type='text' id='date' value='01/01/2011' /> \
-      </p> \
-      <p> \
-        <label for='partidas[0].conta.codigo'>Débito:</label> \
-          <select id='partidas[0].conta.codigo'> \
-            <option value='1.01.0001'>Caixa</option> \
-            <option value='1.02.0001'>Banco</option> \
-            <option value='1.02.0002'>Compras</option> \
-          </select> \
-          </input>  \
-          <input type='text'     id='partidas[0].conta.nome' /> \
-          <input type='hidden'   id='partidas[0].natureza' value='1' /> \
-          <input type='text'   id='partidas[0].nesteds[0].nested' /> \
-          <input type='text'   id='partidas[0].nesteds[1].nested' /> \
-      </p> \
-      <p> \
-        <label for='partidas[1].conta.codigo'>Crédito:</label> \
-          <select id='partidas[1].conta.codigo'> \
-            <option value='1.01.0001'>Caixa</option> \
-            <option value='1.02.0001'>Banco</option> \
-            <option value='1.02.0002'>Compras</option> \
-          </select> \
-          </input>  \
-          <input type='text'   id='partidas[1].conta.nome' /> \
-          <input type='hidden' id='partidas[1].natureza' value='-1' /> \
-          <input type='text'   id='partidas[1].nesteds[0].nested' /> \
-          <input type='text'   id='partidas[1].nesteds[1].nested' /> \
-      </p> \
-      <p> \
-        <label for='description'>Descrição:</label> <textarea id='description'></textarea> \
-      </p>  \
-      <p> \
-        <label for='value'>Valor:</label> <input type='text' id='value' /> \
-      </p> \
-      <p> \
-        <input type='submit' id='submit'/> \
-      </p> \
-    </form>";
+
     
-jQuery(template).appendTo("body");
+jQuery(global["template"]).appendTo("body");
 
 var lancamento;
 
-describe('jsonform', function() {
+describe('Populate with jsonform', function() {
 
     beforeEach( function () {
         lancamento = {
@@ -65,9 +18,17 @@ describe('jsonform', function() {
             date: "12/03/1999"
         };
     });
-    it('when exists nested objects', function () {
+    
+    it('should exists nested objects', function () {
         jQuery('#jsonform').jsonform(lancamento, function(json) {
             expect(jQuery("#partidas\\[0\\]\\.conta\\.codigo").val().toString()).toEqual("1.02.0002");
         });
     });
+    
+    it('should exists nested objects 2', function () {
+        jQuery('#jsonform').jsonform(lancamento, function(json) {
+            expect(jQuery("#partidas\\[0\\]\\.conta\\.codigo").val().toString()).toEqual("1.02.0002");
+        });
+    });
+    
 });
