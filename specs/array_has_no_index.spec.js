43b660dd01dcd3a16dfef516683493bd43fca9fd
AbraaoAlves <abraaoalves@grupofortes.com.br>
2012-01-04 15:46:47 -0300

    [ fixes #12]  fixed

--- specs/array_has_no_index.spec.js
+++ specs/array_has_no_index.spec.js
@@ -4,15 +4,44 @@ describe("Get JSON with jsonform when array has no index", function(){
     beforeEach( function () {
         jQuery("body").html("");
         var template = __dirname + '/templates/array_has_no_index.html';
-        var multiple = require('fs').readFileSync(template).toString()          
+        var multiple = require('fs').readFileSync(template).toString();          
         jQuery(multiple).appendTo("body");
     });
     
     it('should have nested objects', function () {
-        jQuery("[name='partidas[].conta.codigo']").val(["1.01.0001", "1.02.0002"]);
+        jQuery("[name='partidas[].conta.codigo']:first").val(["1.01.0001", "1.02.0002"]);
         var json = jQuery("form[name='jsonform']").getJSON();
         var codigo = json.partidas[0].conta.codigo;                                
         expect(codigo).toEqual([ '1.01.0001', '1.02.0002' ]);
     });
+	
+	 it('should have nested objects with many selects', function () {
+        jQuery("[name='partidas[].conta.codigo']").eq(0).val(["1.01.0001", "1.02.0002"]);
+        jQuery("[name='partidas[].conta.codigo']").eq(1).val(["1.03.0001", "1.03.0002"]);
+        
+        var json = jQuery("form[name='jsonform']").getJSON();
+        
+        var codigoDaContaDaPartida1 = json.partidas[0].conta.codigo;                                
+        var codigoDaContaDaPartida2 = json.partidas[1].conta.codigo;                                
+        
+        expect(codigoDaContaDaPartida1).toEqual([ '1.01.0001', '1.02.0002' ]);
+        expect(codigoDaContaDaPartida2).toEqual([ '1.03.0001', '1.03.0002' ]);
+    });
 
+    it('should have nested objects with many selects (using populate)', function () {
+        var obj = {
+            partidas:[
+                {conta:{codigo:['1.01.0001', '1.02.0002']} },
+                {conta:{codigo:['1.03.0001', '1.03.0002']} }
+            ]
+        };
+        
+        var json = jQuery("form[name='jsonform']").populate(obj).getJSON();
+        
+        var codigoDaContaDaPartida1 = json.partidas[0].conta.codigo;                                
+        var codigoDaContaDaPartida2 = json.partidas[1].conta.codigo;                                
+        
+        expect(codigoDaContaDaPartida1).toEqual([ '1.01.0001', '1.02.0002' ]);
+        expect(codigoDaContaDaPartida2).toEqual([ '1.03.0001', '1.03.0002' ]);
+    });
 });
--- specs/getjson_select_with_multiple.spec.js
+++ specs/getjson_select_with_multiple.spec.js
@@ -4,7 +4,7 @@ describe("Get JSON with jsonform when select is multiple", function(){
     beforeEach( function () {
         jQuery("body").html("");
         var template = __dirname + '/templates/multiple.html';
-        var multiple = require('fs').readFileSync(template).toString()          
+        var multiple = require('fs').readFileSync(template).toString();          
         jQuery(multiple).appendTo("body");
     });
     
@@ -12,6 +12,29 @@ describe("Get JSON with jsonform when select is multiple", function(){
         jQuery("[name='partidas[0].conta.codigo']").val(["1.01.0001", "1.02.0002"]);
         var json = jQuery("form[name='jsonform']").getJSON();
         var codigo = json.partidas[0].conta.codigo;                                
+        expect(codigo).toEqual([ '1.01.0001', '1.02.0002' ]);
+    });
+	
+	it('should have nested objects using method populate', function () {
+        var obj = {
+            partidas:[
+                {conta:{codigo:['1.01.0001', '1.02.0002']} },
+                {conta:{codigo:['1.03.0001', '1.03.0002']} }
+            ]
+        };
+		debugger;
+        console.log("objeto: ", obj);
+		console.log("value element: ", jQuery("form[name='jsonform']").find("[name='partidas[0].conta.codigo']").val());
+		
+        var json = jQuery("form[name='jsonform']").populate(obj).getJSON();
+		
+		console.log("value element: ", jQuery("form[name='jsonform']").find("[name='partidas[0].conta.codigo']").val());
+		
+		console.log("json :", json);
+		console.log("json conta:", json.partidas[0]);
+		
+        var codigo = json.partidas[0].conta.codigo;                                
+        
         expect(codigo).toEqual([ '1.01.0001', '1.02.0002' ]);
     });
 
